6f618c73e502374dda97dc04a1963113bb47e515
Yoji Sekiya <yojisekiya@gmail.com>
2022-09-19 23:36:58 +0900

    create and destory sessions - add auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user 
+    if session[:student_id]
+      @current_user ||= Student.find(session[:student_id])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
