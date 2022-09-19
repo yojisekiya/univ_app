546b5d11564dbc57c72ba4d1b7454cc240badc10
Yoji Sekiya <yojisekiya@gmail.com>
2022-09-19 16:43:51 +0900

    edit student functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,6 +21,20 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
   private
   def student_params
     params.require(:student).permit(:name, :email)
