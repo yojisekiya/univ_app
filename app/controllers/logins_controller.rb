6d03c4ceeabda39efada6bcda7ca8b7b9a5c8183
Yoji Sekiya <yojisekiya@gmail.com>
2022-09-19 22:59:19 +0900

    authentication system - build routes and form

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,11 @@
+class LoginsController < ApplicationController
+  def new
+
+  end
+  def create
+
+  end
+  def destroy 
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "courses#index"
-  get "courses/new", to: "courses#new"
-  get "about", to: "pages#about"
+  root 'courses#index'
+  get 'courses/new', to: 'courses#new'
+  get 'about', to: "pages#about"
   resources :students, except: [:destroy]
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
