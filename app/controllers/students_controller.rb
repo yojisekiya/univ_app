8526270e738fbcfdd352bc23c9066a0379ab7e2d
Yoji Sekiya <yojisekiya@gmail.com>
2022-09-19 16:06:12 +0900

    create students and work with errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,19 @@ class StudentsController < ApplicationController
   def new 
     @student = Student.new
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
 end
